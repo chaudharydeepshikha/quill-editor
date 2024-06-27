a971f8aba06e8c6ab14c0d0a62b1a1663d544806
Deepshikha Chaudhary <deepshikha.chaudhary@zs.com>
2024-06-27 12:03:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { QuillEditorComponent } from "ngx-quill";
+
+import "quill-mention/autoregister";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,132 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  form!: FormGroup;
+  inputValue1: string = '';
+  users = ['Alice', 'Bob', 'Charlie', 'David'];
+  filteredUsers: string[] = [];
+  showAutocomplete = false;
+  @ViewChild('auto') auto!: any;
+
+
+  inputValue?: string;
+  filteredOptions: string[] = [];
+  options = ['Burns Bay Road', 'Downing Street', 'Wall Street'];
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      inputControl: ['']
+    });
+    this.filteredOptions = this.options;
+  }
+
+  onInput(value: any): void {
+    const mentionCharIndex = value.lastIndexOf('@');
+    if (mentionCharIndex !== -1) {
+      const searchString = value.substring(mentionCharIndex + 1);
+      this.filteredUsers = this.users.filter(user => user.toLowerCase().includes(searchString.toLowerCase()));
+      this.showAutocomplete = this.filteredUsers.length > 0;
+    } else {
+      this.showAutocomplete = false;
+    }
+  }
+
+  onSelect(value: any): void {
+    const inputValue = this.form.get('inputControl')?.value;
+    const mentionCharIndex = inputValue.lastIndexOf('@');
+    const newValue = inputValue.substring(0, mentionCharIndex + 1) + value + ' ';
+    this.form.get('inputControl')?.setValue(newValue);
+    this.showAutocomplete = false;
+  }
+
+  onChange(value: string): void {
+    this.filteredOptions = this.options.filter(option => option.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+  }
+
   title = 'quill-editor-demo';
+  editorText: string = '';
+  userList = [
+    { id: '1',value: "Alice Smith",age: '28',email: "alice.smith@example.com" },
+    { id: '2',value: "Bob Johnson",age: '34',email: "bob.johnson@example.com"},
+    { id: '3',value: "Charlie Davis",age: '25',email: "charlie.davis@example.com"},
+    { id: '4',value: "Dana Lee",age: '30',email: "dana.lee@example.com"},
+    { id: '5',value: "Eve Martinez",age: '27',email: "eve.martinez@example.com"}
+  ];
+  @ViewChild(QuillEditorComponent, { static: true })
+  editor!: QuillEditorComponent;
+
+  modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'header': 1 }, { 'header': 2 }],     // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+
+      ['clean']                                         // remove formatting button
+    ],
+    mention: {
+      allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+      showDenotationChar: false,
+      spaceAfterInsert: true,
+      onSelect: (item: any, insertItem: any) => {
+        const editor = this.editor.quillEditor;
+        insertItem(item);
+        // necessary because quill-mention triggers changes as 'api' instead of 'user'
+        editor.insertText(editor.getLength() - 1, "", "user");
+      },
+      source: (searchTerm: any, renderList: any) => {
+        const values = this.userList;
+        console.log(searchTerm);
+        if (searchTerm.length === 0) {
+          renderList(values, searchTerm);
+        } else {
+          const matches : any = [];
+          values.forEach(entry => {
+            if (
+              entry.value.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+            ) {
+              matches.push(entry);
+            }
+          });
+          renderList(matches, searchTerm);
+        }
+      }
+    }
+  }
+
+  getContent() {
+    console.log(this.editorText);
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(this.editorText, 'text/html');
+
+    const mentions = doc.querySelectorAll('.mention');
+
+    const taggedUsers = Array.from(mentions).map(mention => ({
+      id: mention.getAttribute('data-id'),
+      value: mention.getAttribute('data-value'),
+      email: ''
+  }));
+
+  console.log(taggedUsers);
+
+  taggedUsers.map(user => {
+    this.userList.map(u => {
+      if(user.value == u.value) {
+        user.email = u.email;
+      }
+    })
+  })
+  console.log(taggedUsers);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+
+import { QuillModule } from 'ngx-quill';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +25,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    QuillModule.forRoot(),
+    NzInputModule,
+    NzAutocompleteModule,
+    NzDropDownModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
